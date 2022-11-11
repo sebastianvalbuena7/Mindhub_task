e4780f7701016670bd80c7d3b32b7a46a39a8b4a
Sebastian <sebastianvalbuena25@gmail.com>
2022-11-11 18:20:05 -0500

    Task 4

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,21 +1,37 @@
-const info = events
-const params = new URLSearchParams(location.search)
-const id = params.get('id')
-const card = info.find(card => card._id === id)
+let eventsData
+let data
+let date
 
-const div = document.getElementById('details')
-div.innerHTML = `
-    <div class="d-flex flex-column justify-content-center align-items-center height-information border">
-        <div>
-            <img class="img-information px-2 my-4" src="${card.image}" alt="${card.name}">
+fetch("https://amazing-events.herokuapp.com/api/events")
+    .then(response => response.json())
+    .then(json => {
+        eventsData = json
+        data = eventsData.events
+        date = eventsData.currentDate
+        const params = new URLSearchParams(location.search)
+        const id = params.get('id')
+        const card = data.find(card => card._id === id)
+        printDetail(card)
+    })
+    .catch(error => console.error(error))
+
+
+
+function printDetail(cards) {
+    const div = document.getElementById('details')
+    div.innerHTML = `
+        <div class="d-flex flex-column justify-content-center align-items-center height-information border">
+            <div>
+                <img class="img-information px-2 my-4" src="${cards.image}" alt="${cards.name}">
+            </div>
+            <div class="me-2 ms-2">
+                <h3 class="px-3 pt-3">${cards.name}</h3>
+                <p class="fs-5 px-3">Date: ${cards.date}</p>
+                <p class="fs-5 px-3">${cards.description}</p>
+                <p class="fs-5 px-3">Place: ${cards.place}</p>
+                <p class="fs-5 px-3">Capacity: ${cards.capacity}</p>
+                <p class="fs-5 px-3">Assistance: ${cards.assistance ? cards.assistance : cards.estimate}</p>
+            </div>
         </div>
-        <div class="me-2 ms-2">
-            <h3 class="px-3 pt-3">${card.name}</h3>
-            <p class="fs-5 px-3">Date: ${card.date}</p>
-            <p class="fs-5 px-3">${card.description}</p>
-            <p class="fs-5 px-3">Place: ${card.place}</p>
-            <p class="fs-5 px-3">Capacity: ${card.capacity}</p>
-            <p class="fs-5 px-3">Assistance: ${card.assistance ? card.assistance : card.estimate}</p>
-        </div>
-    </div>
-`
+    `
+}
