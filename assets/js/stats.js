dfe9e4e28c7d8cd029b1f0747fcffa66f077c476
Sebastian <sebastianvalbuena25@gmail.com>
2022-11-16 15:48:35 -0500

    Task 4 corregida

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,11 +1,16 @@
 // Variables
 const trHighest = document.getElementById('tr-highest')
+const tbodyUpcoming = document.getElementById('data-upcoming')
+const tbodyPast = document.getElementById('data-past')
 const trCategoryUpcoming = document.getElementById('tr-categoryUpcoming')
 const trCategoryUpcomingRevenues = document.getElementById('tr-categoryUpcomingRevenues')
 const trCategoryUpcomingAttendance = document.getElementById('tr-categoryUpcomingAttendance')
 const trCategoryPast = document.getElementById('tr-categoryPast')
 const trCategoryPastRevenues = document.getElementById('tr-categoryPastRevenues')
 const trCategoryPastAttendance = document.getElementById('tr-categoryPastAttendance')
+let pastEvents
+let upcomingEvents
+let upcomingCategories
 let eventsData
 let data
 let date
@@ -17,16 +22,20 @@ fetch("https://amazing-events.herokuapp.com/api/events")
         eventsData = json
         data = eventsData.events
         date = eventsData.currentDate
+
+        pastEvents = data.filter(event => event.date < eventsData.currentDate)
+        upcomingEvents = data.filter(event => event.date > eventsData.currentDate)
+
+        upcomingCategories = Array.from(new Set(upcomingEvents.map(event => event.category)))
+        pastCategories = Array.from(new Set(pastEvents.map(event => event.category)))
+
         highestAttendance(data)
         lowestAttendance(data)
         largerCapacity(data)
-        const setArray = new Set(data.map( property => property))
-        const arrayCategory = Array.from(setArray).filter(value => value.category !== "Food Fair")
-        const arrayCategory2 = Array.from(setArray).filter(value => value.category)
-        createUpcoming(arrayCategory, date)
-        createPast(arrayCategory2, date)
+        createUpcoming(upcomingCategories, upcomingEvents)
+        createPast(pastCategories, pastEvents)
     })
-    .catch(error => console.error(error))
+    // .catch(error => console.error(error))
 
 // Funciones 
 function highestAttendance(events) {
@@ -94,86 +103,72 @@ function printInfo(event) {
     })
 }
 
-function createUpcoming(events, fetchDate) {
-    // Resultados temporales
-    const resultTemp = {}
-    const resultTemp2 = {}
-    const resultTemp3 = {}
-    // Calcular totales
-    events.map(property => {
-        if(property.date > fetchDate) {
-            const partialResult = resultTemp[property.category]
-            resultTemp[property.category] = partialResult ? partialResult + (property.price * property.estimate)  : property.price
-            const partialResult2 = resultTemp2[property.category]
-            resultTemp2[property.category] = partialResult2 ? partialResult2 + parseInt(property.estimate) : parseInt(property.estimate)
-            const partialResult3 = resultTemp3[property.category]
-            resultTemp3[property.category] = partialResult3 ? partialResult3 + parseInt(property.capacity) : parseInt(property.capacity)
+function createUpcoming(categories, events) {
+    let array = []
+    categories.forEach(property => {
+        let eventsCategory = events.filter(event => event.category === property)
+        
+        let porcentaje = eventsCategory.map(event => (event.estimate * 100 / event.capacity))
+
+        let totals = porcentaje.reduce((a, b) => a + b)
+        let averageAssistance = totals/porcentaje.length
+        
+        let ingresos = eventsCategory.map(event => event.estimate * event.price).reduce((a, b) => a + b)
+
+        const category = {
+            name: property,
+            estimate: averageAssistance.toFixed(0),
+            revenues: ingresos
         }
+        array.push(category)
     })
-    renderUpcoming(resultTemp, resultTemp2, resultTemp3)
+    renderUpcoming(array)
 }
 
-function renderUpcoming(resultTemp, resultTemp2, resultTemp3) {
-    const totals = Object
-    // Guarda las categorias
-    const category = totals.keys(resultTemp, resultTemp2, resultTemp3)
-    const totalResult = category.map(totalResult => (
-    {
-        category: totalResult,
-        price: resultTemp[totalResult],
-        estimate: resultTemp2[totalResult],
-        capacity: resultTemp3[totalResult]
-    }))
-
-    totalResult.map(property => {
-        let infoTd = document.createElement('tr'), infoTd2 = document.createElement('tr'), infoTd3 = document.createElement('tr')
-        infoTd.textContent = `${property.category}`
-        infoTd2.textContent = `$ ${property.price}`
-        infoTd3.textContent = `${Math.trunc(property.estimate * 100 / property.capacity)}%`
-        trCategoryUpcoming.appendChild(infoTd)
-        trCategoryUpcomingRevenues.appendChild(infoTd2)
-        trCategoryUpcomingAttendance.appendChild(infoTd3)
+function renderUpcoming(events) {
+    let template = ''
+    events.forEach((event, index) => {
+        template += `
+        <tr>
+        <td class="table-secondary">${events[index].name}</td>
+        <td class="table-secondary">$${events[index].revenues}</td>
+        <td class="table-secondary">${events[index].assistance || events[index].estimate}%</td>
+        </tr> `
     })
+    tbodyUpcoming.innerHTML = template
 }
 
-function createPast(events, fetchDate) {
-    // Resultados temporales
-    const resultTemp = {}
-    const resultTemp2 = {}
-    const resultTemp3 = {}
-    // Calcular totales
-    events.map(property => {
-        if(property.date < fetchDate) {
-            const partialResult = resultTemp[property.category]
-            resultTemp[property.category] = partialResult ? partialResult + (property.price * property.assistance)  : property.price
-            const partialResult2 = resultTemp2[property.category]
-            resultTemp2[property.category] = partialResult2 ? partialResult2 + parseInt(property.assistance) : parseInt(property.assistance)
-            const partialResult3 = resultTemp3[property.category]
-            resultTemp3[property.category] = partialResult3 ? partialResult3 + parseInt(property.capacity) : parseInt(property.capacity)
+function createPast(categories, events) {
+    let array = []
+    categories.forEach(property => {
+        let eventsCategory = events.filter(event => event.category === property)
+        
+        let porcentaje = eventsCategory.map(event => (event.assistance * 100 / event.capacity))
+
+        let totals = porcentaje.reduce((a, b) => a + b)
+        let averageAssistance = totals/porcentaje.length
+        
+        let ingresos = eventsCategory.map(event => event.assistance * event.price).reduce((a, b) => a + b)
+
+        const category = {
+            name: property,
+            estimate: averageAssistance.toFixed(0),
+            revenues: ingresos
         }
+        array.push(category)
     })
-    renderPast(resultTemp, resultTemp2, resultTemp3)
+    renderPast(array)
 }
 
-function renderPast(resultTemp, resultTemp2, resultTemp3) {
-    const totals = Object
-    // Guarda las categorias
-    const category = totals.keys(resultTemp, resultTemp2, resultTemp3)
-    const totalResult = category.map(totalResult => (
-    {
-        category: totalResult,
-        price: resultTemp[totalResult],
-        assistance: resultTemp2[totalResult],
-        capacity: resultTemp3[totalResult]
-    }))
-
-    totalResult.map(property => {
-        let infoTd = document.createElement('tr'), infoTd2 = document.createElement('tr'), infoTd3 = document.createElement('tr')
-        infoTd.textContent = `${property.category}`
-        infoTd2.textContent = `$ ${property.price}`
-        infoTd3.textContent = `${Math.trunc(property.assistance * 100 / property.capacity)}%`
-        trCategoryPast.appendChild(infoTd)
-        trCategoryPastRevenues.appendChild(infoTd2)
-        trCategoryPastAttendance.appendChild(infoTd3)
+function renderPast(events) {
+    let template = ''
+    events.forEach((event, index) => {
+        template += `
+        <tr>
+        <td class="table-secondary">${events[index].name}</td>
+        <td class="table-secondary">$${events[index].revenues}</td>
+        <td class="table-secondary">${events[index].assistance || events[index].estimate}%</td>
+        </tr> `
     })
+    tbodyPast.innerHTML = template
 }
